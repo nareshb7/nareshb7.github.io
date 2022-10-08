a13e0f6bbeb1d5bc44a999486604cf6c0671d418
nareshb7 <nareshbjava7@gmail.com>
2022-10-08 20:29:09 +0530

    welcomePage

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const projectsEl = document.getElementById('projects')
 const skillsDiv = document.getElementById('skillsDiv')
 const projectsDiv = document.getElementById('projectsDiv')
 const btnClass = document.querySelectorAll('.btn')
-
-let vName = prompt('Please provide Your Name...')
+let vName;
+// let vName = prompt('Please provide Your Name...')
 vName = vName.charAt(0).toUpperCase()+ vName.substring(1)
 vnameEl.innerHTML = `${vName}`
 btnClass.forEach(elem => elem.addEventListener('mouseover', (event)=>{
@@ -31,7 +31,7 @@ const aboutFun =()=>{
     if (outputEl.style.display=='none'){
         outputEl.style.display='block'
         let message = `I am a recently completed Enginnering Graduate who are passionated about 
-        developing web applications`
+        developing web applications <a href="./welcomePage/index.html" >Click to know more</a>`
         outputEl.innerHTML = `${message}`
     }
 }
@@ -50,6 +50,37 @@ const projectsFun=()=>{
         projectsDiv.style.display='block' 
     }
 }
+
+const setCookie = (key, value, days) => {
+    let d = new Date()
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000))
+    let expires = d.toUTCString
+    document.cookie = `${key}=${value};expires=${expires};path=/`
+  }
+  //setCookie('Name', 'Naresh', 30)
+  const getCookie = (key) => {
+    let val = document.cookie.split(';')
+    for (const x of val) {
+      if (x.includes(key)) {
+        let name = x.substring(key.length + 1)
+  
+        return name
+      }
+    }
+  }
+  //getCookie('Name')
+  
+  const checkCookie = () => {
+    let user = getCookie('Name')
+    vName= user;
+    if (user) {
+      alert(`Welcome again ${user}`)
+    } else {
+      let name = prompt('Enter your name')
+      setCookie('Name', name, 30)
+    }
+  }
+  checkCookie()
 
 projectsEl.onclick = ()=> projectsFun()
 aboutEl.onclick =()=> aboutFun()
